c2a0073ce9a8799fe3ae62e39907730f8967e45b
hrwangchengdu <hrwangchengdu@hotmail.com>
2018-12-19 04:46:10 +0000

    Update mvn

--- mvn.sh
+++ mvn.sh
@@ -3,9 +3,9 @@ wget http://mirror.olnevhost.net/pub/apache/maven/maven-3/3.0.5/binaries/apache-
 tar xvf apache-maven-3.0.5-bin.tar.gz
 sudo mv apache-maven-3.0.5  /usr/local/apache-maven
 
-echo export M2_HOME=/usr/local/apache-maven >> ~/.bashrc
-echo export M2=$M2_HOME/bin >> ~/.bashrc
-echo export PATH=$M2:$PATH >> ~/.bashrc
+echo "export M2_HOME=/usr/local/apache-maven" >> ~/.bashrc
+echo "export M2=\$M2_HOME/bin" >> ~/.bashrc
+echo "export PATH=\$M2:\$PATH" >> ~/.bashrc
 
 source ~/.bashrc
 echo "show mvn info"
